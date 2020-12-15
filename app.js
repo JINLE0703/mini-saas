4abf9f4cdce28953ee2a6f699fa42310e92792f6
qiushuo <qiushuo@do1.com.cn>
2020-12-15 11:14:12 +0800

    perf: 优化icon和字体

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // app.js
 App({
   onLaunch (options) {
-    // Do something initial when launch.
+    // 加载字体
+    wx.loadFontFace({
+      global: true,
+      family: 'tx-font',
+      source: 'https://6465-demo-1gkm3ya7d1739416-1304452149.tcb.qcloud.la/font/%E8%85%BE%E8%AE%AF%E4%BD%93-W7.otf?sign=907e67c2f16bfcae108b66d8f9565cbb&t=1607946452',
+    })
+    wx.loadFontFace({
+      global: true,
+      family: 'iconfont',
+      source: 'https://at.alicdn.com/t/font_2270683_7idkshohwif.woff2',
+    })
   },
   onShow (options) {
     // Do something when show.
